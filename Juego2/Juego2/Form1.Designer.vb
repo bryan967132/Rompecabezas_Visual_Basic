198f8dd2a209c41457622438a6e240779dec09ef
bryan967132 <81400332+bryan967132@users.noreply.github.com>
2022-08-04 22:10:56 -0600

    Juego 2

---
+++ Juego2/Juego2/Form1.Designer.vb
@@ -0,0 +1,216 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox6 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox7 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox8 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox9 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox10 = New System.Windows.Forms.PictureBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ImageList1 = New System.Windows.Forms.ImageList(Me.components)
+        Me.ImageList2 = New System.Windows.Forms.ImageList(Me.components)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox10, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Gray
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(120, 80)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackColor = System.Drawing.Color.Gray
+        Me.PictureBox2.Location = New System.Drawing.Point(138, 12)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(120, 80)
+        Me.PictureBox2.TabIndex = 1
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.BackColor = System.Drawing.Color.Gray
+        Me.PictureBox3.Location = New System.Drawing.Point(264, 12)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(120, 80)
+        Me.PictureBox3.TabIndex = 2
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.BackColor = System.Drawing.Color.Gray
+        Me.PictureBox4.Location = New System.Drawing.Point(12, 98)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(120, 80)
+        Me.PictureBox4.TabIndex = 5
+        Me.PictureBox4.TabStop = False
+        '
+        'PictureBox5
+        '
+        Me.PictureBox5.BackColor = System.Drawing.Color.Gray
+        Me.PictureBox5.Location = New System.Drawing.Point(138, 98)
+        Me.PictureBox5.Name = "PictureBox5"
+        Me.PictureBox5.Size = New System.Drawing.Size(120, 80)
+        Me.PictureBox5.TabIndex = 4
+        Me.PictureBox5.TabStop = False
+        '
+        'PictureBox6
+        '
+        Me.PictureBox6.BackColor = System.Drawing.Color.Gray
+        Me.PictureBox6.Location = New System.Drawing.Point(264, 98)
+        Me.PictureBox6.Name = "PictureBox6"
+        Me.PictureBox6.Size = New System.Drawing.Size(120, 80)
+        Me.PictureBox6.TabIndex = 3
+        Me.PictureBox6.TabStop = False
+        '
+        'PictureBox7
+        '
+        Me.PictureBox7.BackColor = System.Drawing.Color.Gray
+        Me.PictureBox7.Location = New System.Drawing.Point(12, 184)
+        Me.PictureBox7.Name = "PictureBox7"
+        Me.PictureBox7.Size = New System.Drawing.Size(120, 80)
+        Me.PictureBox7.TabIndex = 8
+        Me.PictureBox7.TabStop = False
+        '
+        'PictureBox8
+        '
+        Me.PictureBox8.BackColor = System.Drawing.Color.Gray
+        Me.PictureBox8.Location = New System.Drawing.Point(138, 184)
+        Me.PictureBox8.Name = "PictureBox8"
+        Me.PictureBox8.Size = New System.Drawing.Size(120, 80)
+        Me.PictureBox8.TabIndex = 7
+        Me.PictureBox8.TabStop = False
+        '
+        'PictureBox9
+        '
+        Me.PictureBox9.BackColor = System.Drawing.Color.White
+        Me.PictureBox9.Location = New System.Drawing.Point(264, 184)
+        Me.PictureBox9.Name = "PictureBox9"
+        Me.PictureBox9.Size = New System.Drawing.Size(120, 80)
+        Me.PictureBox9.TabIndex = 6
+        Me.PictureBox9.TabStop = False
+        '
+        'PictureBox10
+        '
+        Me.PictureBox10.Location = New System.Drawing.Point(420, 12)
+        Me.PictureBox10.Name = "PictureBox10"
+        Me.PictureBox10.Size = New System.Drawing.Size(372, 252)
+        Me.PictureBox10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox10.TabIndex = 9
+        Me.PictureBox10.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(346, 289)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(120, 37)
+        Me.Button1.TabIndex = 10
+        Me.Button1.Text = "Nuevo Juego"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ImageList1
+        '
+        Me.ImageList1.ImageStream = CType(resources.GetObject("ImageList1.ImageStream"), System.Windows.Forms.ImageListStreamer)
+        Me.ImageList1.TransparentColor = System.Drawing.Color.Transparent
+        Me.ImageList1.Images.SetKeyName(0, "ImgB.jpeg")
+        Me.ImageList1.Images.SetKeyName(1, "Img1.jpeg")
+        Me.ImageList1.Images.SetKeyName(2, "Img2.jpeg")
+        Me.ImageList1.Images.SetKeyName(3, "Img3.jpeg")
+        Me.ImageList1.Images.SetKeyName(4, "Img4.jpeg")
+        Me.ImageList1.Images.SetKeyName(5, "Img5.jpeg")
+        Me.ImageList1.Images.SetKeyName(6, "Img6.jpeg")
+        Me.ImageList1.Images.SetKeyName(7, "Img7.jpeg")
+        Me.ImageList1.Images.SetKeyName(8, "Img8.jpeg")
+        '
+        'ImageList2
+        '
+        Me.ImageList2.ImageStream = CType(resources.GetObject("ImageList2.ImageStream"), System.Windows.Forms.ImageListStreamer)
+        Me.ImageList2.TransparentColor = System.Drawing.Color.Transparent
+        Me.ImageList2.Images.SetKeyName(0, "Paisaje.jpeg")
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(804, 338)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox10)
+        Me.Controls.Add(Me.PictureBox7)
+        Me.Controls.Add(Me.PictureBox8)
+        Me.Controls.Add(Me.PictureBox9)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.PictureBox5)
+        Me.Controls.Add(Me.PictureBox6)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox10, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox3 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox4 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox5 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox6 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox7 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox8 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox9 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox10 As System.Windows.Forms.PictureBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents ImageList1 As System.Windows.Forms.ImageList
+    Friend WithEvents ImageList2 As System.Windows.Forms.ImageList
+
+End Class
---
+++ Juego2/Juego2/Form1.vb
@@ -0,0 +1,186 @@
+﻿Public Class Form1
+    Private Sub PictureBox1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PictureBox1.Click
+        If PictureBox2.BackColor = Color.White Then
+            PictureBox2.Image = PictureBox1.Image
+            PictureBox2.BackColor = Color.Gray
+            PictureBox1.Image = ImageList1.Images(0)
+            PictureBox1.BackColor = Color.White
+        End If
+        If PictureBox4.BackColor = Color.White Then
+            PictureBox4.Image = PictureBox1.Image
+            PictureBox4.BackColor = Color.Gray
+            PictureBox1.Image = ImageList1.Images(0)
+            PictureBox1.BackColor = Color.White
+        End If
+    End Sub
+    Private Sub PictureBox2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PictureBox2.Click
+        If PictureBox1.BackColor = Color.White Then
+            PictureBox1.Image = PictureBox2.Image
+            PictureBox1.BackColor = Color.Gray
+            PictureBox2.Image = ImageList1.Images(0)
+            PictureBox2.BackColor = Color.White
+        End If
+        If PictureBox3.BackColor = Color.White Then
+            PictureBox3.Image = PictureBox2.Image
+            PictureBox3.BackColor = Color.Gray
+            PictureBox2.Image = ImageList1.Images(0)
+            PictureBox2.BackColor = Color.White
+        End If
+        If PictureBox5.BackColor = Color.White Then
+            PictureBox5.Image = PictureBox2.Image
+            PictureBox5.BackColor = Color.Gray
+            PictureBox2.Image = ImageList1.Images(0)
+            PictureBox2.BackColor = Color.White
+        End If
+    End Sub
+    Private Sub PictureBox3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PictureBox3.Click
+        If PictureBox2.BackColor = Color.White Then
+            PictureBox2.Image = PictureBox3.Image
+            PictureBox2.BackColor = Color.Gray
+            PictureBox3.Image = ImageList1.Images(0)
+            PictureBox3.BackColor = Color.White
+        End If
+        If PictureBox6.BackColor = Color.White Then
+            PictureBox6.Image = PictureBox3.Image
+            PictureBox6.BackColor = Color.Gray
+            PictureBox3.Image = ImageList1.Images(0)
+            PictureBox3.BackColor = Color.White
+        End If
+    End Sub
+    Private Sub PictureBox4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PictureBox4.Click
+        If PictureBox1.BackColor = Color.White Then
+            PictureBox1.Image = PictureBox4.Image
+            PictureBox1.BackColor = Color.Gray
+            PictureBox4.Image = ImageList1.Images(0)
+            PictureBox4.BackColor = Color.White
+        End If
+        If PictureBox7.BackColor = Color.White Then
+            PictureBox7.Image = PictureBox4.Image
+            PictureBox7.BackColor = Color.Gray
+            PictureBox4.Image = ImageList1.Images(0)
+            PictureBox4.BackColor = Color.White
+        End If
+        If PictureBox5.BackColor = Color.White Then
+            PictureBox5.Image = PictureBox4.Image
+            PictureBox5.BackColor = Color.Gray
+            PictureBox4.Image = ImageList1.Images(0)
+            PictureBox4.BackColor = Color.White
+        End If
+    End Sub
+    Private Sub PictureBox5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PictureBox5.Click
+        If PictureBox4.BackColor = Color.White Then
+            PictureBox4.Image = PictureBox5.Image
+            PictureBox4.BackColor = Color.Gray
+            PictureBox5.Image = ImageList1.Images(0)
+            PictureBox5.BackColor = Color.White
+        End If
+        If PictureBox6.BackColor = Color.White Then
+            PictureBox6.Image = PictureBox5.Image
+            PictureBox6.BackColor = Color.Gray
+            PictureBox5.Image = ImageList1.Images(0)
+            PictureBox5.BackColor = Color.White
+        End If
+        If PictureBox2.BackColor = Color.White Then
+            PictureBox2.Image = PictureBox5.Image
+            PictureBox2.BackColor = Color.Gray
+            PictureBox5.Image = ImageList1.Images(0)
+            PictureBox5.BackColor = Color.White
+        End If
+        If PictureBox8.BackColor = Color.White Then
+            PictureBox8.Image = PictureBox5.Image
+            PictureBox8.BackColor = Color.Gray
+            PictureBox5.Image = ImageList1.Images(0)
+            PictureBox5.BackColor = Color.White
+        End If
+    End Sub
+    Private Sub PictureBox6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PictureBox6.Click
+        If PictureBox5.BackColor = Color.White Then
+            PictureBox5.Image = PictureBox6.Image
+            PictureBox5.BackColor = Color.Gray
+            PictureBox6.Image = ImageList1.Images(0)
+            PictureBox6.BackColor = Color.White
+        End If
+        If PictureBox3.BackColor = Color.White Then
+            PictureBox3.Image = PictureBox6.Image
+            PictureBox3.BackColor = Color.Gray
+            PictureBox6.Image = ImageList1.Images(0)
+            PictureBox6.BackColor = Color.White
+        End If
+        If PictureBox9.BackColor = Color.White Then
+            PictureBox9.Image = PictureBox6.Image
+            PictureBox9.BackColor = Color.Gray
+            PictureBox6.Image = ImageList1.Images(0)
+            PictureBox6.BackColor = Color.White
+        End If
+    End Sub
+    Private Sub PictureBox7_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PictureBox7.Click
+        If PictureBox4.BackColor = Color.White Then
+            PictureBox4.Image = PictureBox7.Image
+            PictureBox4.BackColor = Color.Gray
+            PictureBox7.Image = ImageList1.Images(0)
+            PictureBox7.BackColor = Color.White
+        End If
+        If PictureBox8.BackColor = Color.White Then
+            PictureBox8.Image = PictureBox7.Image
+            PictureBox8.BackColor = Color.Gray
+            PictureBox7.Image = ImageList1.Images(0)
+            PictureBox7.BackColor = Color.White
+        End If
+    End Sub
+    Private Sub PictureBox8_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PictureBox8.Click
+        If PictureBox7.BackColor = Color.White Then
+            PictureBox7.Image = PictureBox8.Image
+            PictureBox7.BackColor = Color.Gray
+            PictureBox8.Image = ImageList1.Images(0)
+            PictureBox8.BackColor = Color.White
+        End If
+        If PictureBox5.BackColor = Color.White Then
+            PictureBox5.Image = PictureBox8.Image
+            PictureBox5.BackColor = Color.Gray
+            PictureBox8.Image = ImageList1.Images(0)
+            PictureBox8.BackColor = Color.White
+        End If
+        If PictureBox9.BackColor = Color.White Then
+            PictureBox9.Image = PictureBox8.Image
+            PictureBox9.BackColor = Color.Gray
+            PictureBox8.Image = ImageList1.Images(0)
+            PictureBox8.BackColor = Color.White
+        End If
+    End Sub
+    Private Sub PictureBox9_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PictureBox9.Click
+        If PictureBox8.BackColor = Color.White Then
+            PictureBox8.Image = PictureBox9.Image
+            PictureBox8.BackColor = Color.Gray
+            PictureBox9.Image = ImageList1.Images(0)
+            PictureBox9.BackColor = Color.White
+        End If
+        If PictureBox6.BackColor = Color.White Then
+            PictureBox6.Image = PictureBox9.Image
+            PictureBox6.BackColor = Color.Gray
+            PictureBox9.Image = ImageList1.Images(0)
+            PictureBox9.BackColor = Color.White
+        End If
+    End Sub
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        PictureBox1.Image = ImageList1.Images(7)
+        PictureBox2.Image = ImageList1.Images(5)
+        PictureBox3.Image = ImageList1.Images(1)
+        PictureBox4.Image = ImageList1.Images(8)
+        PictureBox5.Image = ImageList1.Images(6)
+        PictureBox6.Image = ImageList1.Images(2)
+        PictureBox7.Image = ImageList1.Images(3)
+        PictureBox8.Image = ImageList1.Images(4)
+        PictureBox9.Image = ImageList1.Images(0)
+        PictureBox10.Image = ImageList2.Images(0)
+
+        PictureBox1.BackColor = Color.Gray
+        PictureBox2.BackColor = Color.Gray
+        PictureBox3.BackColor = Color.Gray
+        PictureBox4.BackColor = Color.Gray
+        PictureBox5.BackColor = Color.Gray
+        PictureBox6.BackColor = Color.Gray
+        PictureBox7.BackColor = Color.Gray
+        PictureBox8.BackColor = Color.Gray
+        PictureBox9.BackColor = Color.White
+    End Sub
+End Class
